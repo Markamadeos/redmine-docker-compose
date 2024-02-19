d58368a76e328891d842ac3a0fa33ee164d26b04
Mark Quarasique <julipaha@gmail.com>
2024-02-19 13:31:46 +0300

    Корректирует конфиг

--- backup.sh
+++ backup.sh
@@ -1,25 +1,18 @@
+
 #!/bin/sh
-set -e
 
-ROOT_DIR_NAME=backups
-SUB_DIR_NAME=`date +%Y%m%d`
-FILE_PREFIX=`date +%H%M%S`
-IMAGE_BASE_NAME=`basename $(pwd) | tr -d "-"`
-
-echo "[INFO] Start backup."
-
-if [ ! -d ./${ROOT_DIR_NAME}/${SUB_DIR_NAME} ]; then
-  mkdir ./${ROOT_DIR_NAME}/${SUB_DIR_NAME}
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <backup folder>";
+    exit;
 fi
 
-# TODO : Can create safe backup option (Stop all containers befor backup).
-sudo docker run --volumes-from mariadb-storage -v $(pwd)/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/:/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/ \
-  ${IMAGE_BASE_NAME}_mariadb-storage:latest tar cvf /${ROOT_DIR_NAME}/${SUB_DIR_NAME}/${FILE_PREFIX}_data_mariadb.tar /var/lib/mysql
+# current dir
+cd=${0%/*}
 
-sudo docker run --volumes-from redmine-files -v $(pwd)/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/:/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/ \
-  ${IMAGE_BASE_NAME}_redmine-files:latest tar cvf /${ROOT_DIR_NAME}/${SUB_DIR_NAME}/${FILE_PREFIX}_files_redmine.tar /usr/src/app/redmine/files
+. $cd/.env
 
-sudo docker run --volumes-from git-storage -v $(pwd)/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/:/${ROOT_DIR_NAME}/${SUB_DIR_NAME}/ \
-  ${IMAGE_BASE_NAME}_git-storage:latest tar cvf /${ROOT_DIR_NAME}/${SUB_DIR_NAME}/${FILE_PREFIX}_git_repositories.tar /usr/src/git
+docker run --rm --network ${DOCKER_NETWORK} -it mariadb:${MARIADB_VERSION} mysqldump --host ${MYSQL_HOST} --user=root --password=${MYSQL_PWD} --opt ${REDMINE_DB} > /data/redmine/redmine.sql
+
+tar -C / -cz -f $1/redmine.tar.gz --exclude="db/*" data/redmine opt/redmine
 
 echo "[INFO] Finish backup."
